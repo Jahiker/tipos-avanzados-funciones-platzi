491ca7c32f975cbe5a710b10189d4a6345efa8f3
jahiker <rojasjahiker@gmail.com>
2023-03-09 17:30:18 -0400

    [add] DTOs

--- src/app/main.ts
+++ src/app/main.ts
@@ -3,7 +3,6 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.datatype.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.imageUrl(),
     color: faker.color.rgb(),
@@ -11,16 +10,9 @@ for (let index = 0; index < 50; index++) {
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElement(),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
     stock: faker.datatype.number({ min: 10, max: 100 }),
     size: faker.helpers.arrayElement(['S', 'M', 'L', 'XL']),
-    category: {
-      id: faker.datatype.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    },
+    categoryId: faker.datatype.uuid(),
   });
 }
 
